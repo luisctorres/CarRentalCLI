cb1287d3a1a99d27184e1bf8d7c96772679774bd
luisctorres <Scratch2c@gmail.com>
2022-02-01 19:16:29 -0500

    for loop is now working to display available vehicles.

--- src/com/careerdevs/RentalService.java
+++ src/com/careerdevs/RentalService.java
@@ -2,15 +2,20 @@ package com.careerdevs;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class RentalService {
+
+    //Scanner
+    public Scanner userMenuInput = new Scanner(System.in);
 
     public static void main(String[] args) {
         //Print Greeting
         System.out.println("Welcome to the Car Rental CLI");
 
         //Empty array to store cars into
-        ArrayList<Car> carStorage = new ArrayList<>();
+        ArrayList<Car> availableCars = new ArrayList<>();
+        ArrayList<Car> rentedCars = new ArrayList<>();
 
         //Call constructor to create instances of Car
         Car toyotaCamry = new Car("Toyota", "Camry", false);
@@ -18,22 +23,21 @@ public class RentalService {
         Car chevyImpala = new Car("Chevy", "Impala", false);
 
         //adding all cars into carStorage
-        carStorage.add(0, toyotaCamry);
-        carStorage.add(1, hondaAccord);
-        carStorage.add(2, chevyImpala);
+        availableCars.add(0, toyotaCamry);
+        availableCars.add(1, hondaAccord);
+        availableCars.add(2, chevyImpala);
 
-        System.out.println("Available Cars: " + carStorage);
-//        System.out.println("Available Cars");
-//        for (int i = 0; i < carStorage.size(); i++) {
-//            System.out.println(Arrays.toString(carStorage.get(i)));
-//        }
+        System.out.println("Available Cars: ");
+        for (int i = 0; i < availableCars.size(); i++) {
+            System.out.println(availableCars.get(i).make + " " + availableCars.get(i).model);
+        }
+
+
     }
-
-
-
-
-
-
-
-
 }
+
+
+//To-do
+// 1) Need to make a mainMenu() Method
+// 2) need to ask user for their choice
+// 3) Once selection is made. Move car to rented ArrayList and change isRented (Property?) to = True
